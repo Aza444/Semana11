d38a5eddef772b39ed95c66f82a4a9aefb3f99bb
Aza444 <125324304+Aza444@users.noreply.github.com>
2023-10-15 21:43:21 -0600

    Add files via upload

---
+++ Sql/Sql/Context.cs
@@ -0,0 +1,17 @@
+﻿
+using GuiaSemana11EntityFramework;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class Context : DbContext
+{
+
+    public DbSet<EstudianteUNAB> Estudiantes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-AD7C6T5\\SQLEXPRESS;Database=Program2;Trusted_Connection=True;");
+        }
+    }
+}
